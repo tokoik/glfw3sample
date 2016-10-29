4c30bdf81e381f5964233e86ec95d7da897b851b
Kohe Tokoi <tokoik@gmail.com>
2016-10-30 01:37:35 +0900

    step08

--- Window.h
+++ Window.h
@@ -8,14 +8,18 @@ class Window
   // ウィンドウの識別子
   GLFWwindow *const window;
 
-  // 縦横比
-  GLfloat aspect;
+  // ウィンドウのサイズ
+  GLfloat size[2];
+
+  // ワールド座標系に対するデバイス座標系の拡大率
+  GLfloat scale;
 
 public:
 
   // コンストラクタ
   Window(int width = 640, int height = 480, const char *title = "Hello!")
     : window(glfwCreateWindow(width, height, title, NULL, NULL))
+    , scale(100.0f)
   {
     if (window == NULL)
     {
@@ -89,12 +93,15 @@ public:
 
     if (instance != NULL)
     {
-      // このインスタンスが保持する縦横比を更新する
-      instance->aspect =
-        static_cast<GLfloat>(width) / static_cast<GLfloat>(height);
+      // 開いたウィンドウのサイズを保存する
+      instance->size[0] = static_cast<GLfloat>(width);
+      instance->size[1] = static_cast<GLfloat>(height);
     }
   }
 
-  // 縦横比を取り出す
-  GLfloat getAspect() const { return aspect; }
+  // ウィンドウのサイズを取り出す
+  const GLfloat *getSize() const { return size; }
+
+  // ワールド座標系に対するデバイス座標系の拡大率を取り出す
+  GLfloat getScale() const { return scale; }
 };
--- point.vert
+++ point.vert
@@ -1,7 +1,8 @@
 #version 150 core
-uniform float aspect;
+uniform vec2 size;
+uniform float scale;
 in vec4 position;
 void main()
 {
-  gl_Position = vec4(1.0 / aspect, 1.0, 1.0, 1.0) * position;
+  gl_Position = vec4(2.0 * scale / size, 1.0, 1.0) * position;
 }
