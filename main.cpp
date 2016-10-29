cdb5212730a97416276bf995c013f28138a71355
Kohe Tokoi <tokoik@gmail.com>
2016-10-30 01:37:34 +0900

    step02

--- main.cpp
+++ main.cpp
@@ -3,6 +3,47 @@
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
 
+// プログラムオブジェクトを作成する
+//   vsrc: バーテックスシェーダのソースプログラムの文字列
+//   fsrc: フラグメントシェーダのソースプログラムの文字列
+GLuint createProgram(const char *vsrc, const char *fsrc)
+{
+  // 空のプログラムオブジェクトを作成する
+  const GLuint program(glCreateProgram());
+
+  if (vsrc != NULL)
+  {
+    // バーテックスシェーダのシェーダオブジェクトを作成する
+    const GLuint vobj(glCreateShader(GL_VERTEX_SHADER));
+    glShaderSource(vobj, 1, &vsrc, NULL);
+    glCompileShader(vobj);
+
+    // バーテックスシェーダのシェーダオブジェクトをプログラムオブジェクトに組み込む
+    glAttachShader(program, vobj);
+    glDeleteShader(vobj);
+  }
+
+  if (fsrc != NULL)
+  {
+    // フラグメントシェーダのシェーダオブジェクトを作成する
+    const GLuint fobj(glCreateShader(GL_FRAGMENT_SHADER));
+    glShaderSource(fobj, 1, &fsrc, NULL);
+    glCompileShader(fobj);
+
+    // フラグメントシェーダのシェーダオブジェクトをプログラムオブジェクトに組み込む
+    glAttachShader(program, fobj);
+    glDeleteShader(fobj);
+  }
+
+  // プログラムオブジェクトをリンクする
+  glBindAttribLocation(program, 0, "position");
+  glBindFragDataLocation(program, 0, "fragment");
+  glLinkProgram(program);
+
+  // 作成したプログラムオブジェクトを返す
+  return program;
+}
+
 int main()
 {
   // GLFW を初期化する
@@ -48,11 +89,35 @@ int main()
   // 背景色を指定する
   glClearColor(1.0f, 1.0f, 1.0f, 0.0f);
 
+  // バーテックスシェーダのソースプログラム
+  static constexpr GLchar vsrc[] =
+    "#version 150 core\n"
+    "in vec4 position;\n"
+    "void main(void)\n"
+    "{\n"
+    "  gl_Position = position;\n"
+    "}\n";
+
+  // フラグメントシェーダのソースプログラム
+  static constexpr GLchar fsrc[] =
+    "#version 150 core\n"
+    "out vec4 fragment;\n"
+    "void main(void)\n"
+    "{\n"
+    "  fragment = vec4(1.0, 0.0, 0.0, 1.0);\n"
+    "}\n";
+
+  // プログラムオブジェクトを作成する
+  const GLuint program(createProgram(vsrc, fsrc));
+
   // ウィンドウが開いている間繰り返す
   while (glfwWindowShouldClose(window) == GL_FALSE)
   {
     // ウィンドウを消去する
     glClear(GL_COLOR_BUFFER_BIT);
+
+    // シェーダプログラムの使用開始
+    glUseProgram(program);
 
     //
     // ここで描画処理を行う
