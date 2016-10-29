b807523a501a452c6025e4f956dfaf4975cd604f
Kohe Tokoi <tokoik@gmail.com>
2016-10-30 01:37:34 +0900

    step03

--- main.cpp
+++ main.cpp
@@ -1,7 +1,59 @@
 ﻿#include <cstdlib>
 #include <iostream>
+#include <vector>
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
+
+// シェーダオブジェクトのコンパイル結果を表示する
+//   shader: シェーダオブジェクト名
+//   str: コンパイルエラーが発生した場所を示す文字列
+GLboolean printShaderInfoLog(GLuint shader, const char *str)
+{
+  // コンパイル結果を取得する
+  GLint status;
+  glGetShaderiv(shader, GL_COMPILE_STATUS, &status);
+  if (status == GL_FALSE) std::cerr << "Compile Error in " << str << std::endl;
+
+  // シェーダのコンパイル時のログの長さを取得する
+  GLsizei bufSize;
+  glGetShaderiv(shader, GL_INFO_LOG_LENGTH, &bufSize);
+
+  if (bufSize > 1)
+  {
+    // シェーダのコンパイル時のログの内容を取得する
+    std::vector<GLchar> infoLog(bufSize);
+    GLsizei length;
+    glGetShaderInfoLog(shader, bufSize, &length, &infoLog[0]);
+    std::cerr << &infoLog[0] << std::endl;
+  }
+
+  return static_cast<GLboolean>(status);
+}
+
+// プログラムオブジェクトのリンク結果を表示する
+//   program: プログラムオブジェクト名
+GLboolean printProgramInfoLog(GLuint program)
+{
+  // リンク結果を取得する
+  GLint status;
+  glGetProgramiv(program, GL_LINK_STATUS, &status);
+  if (status == GL_FALSE) std::cerr << "Link Error." << std::endl;
+
+  // シェーダのリンク時のログの長さを取得する
+  GLsizei bufSize;
+  glGetProgramiv(program, GL_INFO_LOG_LENGTH, &bufSize);
+
+  if (bufSize > 1)
+  {
+    // シェーダのリンク時のログの内容を取得する
+    std::vector<GLchar> infoLog(bufSize);
+    GLsizei length;
+    glGetProgramInfoLog(program, bufSize, &length, &infoLog[0]);
+    std::cerr << &infoLog[0] << std::endl;
+  }
+
+  return static_cast<GLboolean>(status);
+}
 
 // プログラムオブジェクトを作成する
 //   vsrc: バーテックスシェーダのソースプログラムの文字列
@@ -19,7 +71,8 @@ GLuint createProgram(const char *vsrc, const char *fsrc)
     glCompileShader(vobj);
 
     // バーテックスシェーダのシェーダオブジェクトをプログラムオブジェクトに組み込む
-    glAttachShader(program, vobj);
+    if (printShaderInfoLog(vobj, "vertex shader"))
+      glAttachShader(program, vobj);
     glDeleteShader(vobj);
   }
 
@@ -31,7 +84,8 @@ GLuint createProgram(const char *vsrc, const char *fsrc)
     glCompileShader(fobj);
 
     // フラグメントシェーダのシェーダオブジェクトをプログラムオブジェクトに組み込む
-    glAttachShader(program, fobj);
+    if (printShaderInfoLog(fobj, "fragment shader"))
+      glAttachShader(program, fobj);
     glDeleteShader(fobj);
   }
 
@@ -41,7 +95,12 @@ GLuint createProgram(const char *vsrc, const char *fsrc)
   glLinkProgram(program);
 
   // 作成したプログラムオブジェクトを返す
-  return program;
+  if (printProgramInfoLog(program))
+    return program;
+
+  // プログラムオブジェクトが作成できなければ 0 を返す
+  glDeleteProgram(program);
+  return 0;
 }
 
 int main()
