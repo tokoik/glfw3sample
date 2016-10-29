11ea272f01ed47a3b964b45205c30c18706905d2
Kohe Tokoi <tokoik@gmail.com>
2016-10-30 01:37:36 +0900

    step21

---
+++ SolidShapeIndex.h
@@ -0,0 +1,32 @@
+﻿#pragma once
+
+// インデックスを使った図形の描画
+#include "ShapeIndex.h"
+
+//
+// インデックスを使った三角形による描画
+//
+class SolidShapeIndex
+  : public ShapeIndex
+{
+public:
+
+  // コンストラクタ
+  //   size: 頂点の位置の次元
+  //   vertexcount: 頂点の数
+  //   vertex: 頂点属性を格納した配列
+  //   indexcount: 頂点のインデックスの要素数
+  //   index: 頂点のインデックスを格納した配列
+  SolidShapeIndex(GLint size, GLsizei vertexcount, const Object::Vertex *vertex,
+    GLsizei indexcount, const GLuint *index)
+    : ShapeIndex(size, vertexcount, vertex, indexcount, index)
+  {
+  }
+
+  // 描画の実行
+  virtual void execute() const
+  {
+    // 三角形で描画する
+    glDrawElements(GL_TRIANGLES, indexcount, GL_UNSIGNED_INT, 0);
+  }
+};
