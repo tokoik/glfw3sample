e2c21cc0a8c20a7066c00cb64735aaee79a81015
Kohe Tokoi <tokoik@gmail.com>
2016-10-30 01:37:34 +0900

    step06

---
+++ Window.h
@@ -0,0 +1,80 @@
+﻿#pragma once
+
+//
+// ウィンドウ関連の処理
+//
+class Window
+{
+  // ウィンドウの識別子
+  GLFWwindow *const window;
+
+public:
+
+  // コンストラクタ
+  Window(int width = 640, int height = 480, const char *title = "Hello!")
+    : window(glfwCreateWindow(width, height, title, NULL, NULL))
+  {
+    if (window == NULL)
+    {
+      // ウィンドウが作成できなかった
+      std::cerr << "Can't create GLFW window." << std::endl;
+      exit(1);
+    }
+
+    // 現在のウィンドウを処理対象にする
+    glfwMakeContextCurrent(window);
+
+    // GLEW を初期化する
+    glewExperimental = GL_TRUE;
+    if (glewInit() != GLEW_OK)
+    {
+      // GLEW の初期化に失敗した
+      std::cerr << "Can't initialize GLEW" << std::endl;
+      exit(1);
+    }
+
+    // 作成したウィンドウに対する設定
+    glfwSwapInterval(1);
+
+    // ウィンドウのサイズ変更時に呼び出す処理の登録
+    glfwSetWindowSizeCallback(window, resize);
+
+    // 開いたウィンドウの初期設定
+    resize(window, width, height);
+  }
+
+  // デストラクタ
+  virtual ~Window()
+  {
+    // ウィンドウを破棄する
+    glfwDestroyWindow(window);
+  }
+
+  // 描画ループの継続判定
+  explicit operator bool()
+  {
+    // イベントを取り出す
+    glfwWaitEvents();
+
+    // ウィンドウを閉じる必要がなければ true を返す
+    return !glfwWindowShouldClose(window);
+  }
+
+  // ダブルバッファリング
+  void swapBuffers() const
+  {
+    // カラーバッファを入れ替える
+    glfwSwapBuffers(window);
+  }
+
+  // ウィンドウのサイズ変更時の処理
+  static void resize(GLFWwindow *window, int width, int height)
+  {
+    // フレームバッファの大きさを得る
+    int fbWidth, fbHeight;
+    glfwGetFramebufferSize(window, &fbWidth, &fbHeight);
+    
+    // フレームバッファ全体をビューポートにする
+    glViewport(0, 0, fbWidth, fbHeight);
+  }
+};
