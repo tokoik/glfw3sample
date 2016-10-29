fdd1d86989e80354a3d2016e4ea3038aeb62ee18
Kohe Tokoi <tokoik@gmail.com>
2016-10-30 01:37:34 +0900

    step06

--- main.cpp
+++ main.cpp
@@ -5,6 +5,7 @@
 #include <memory>
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
+#include "Window.h"
 #include "Shape.h"
 
 // シェーダオブジェクトのコンパイル結果を表示する
@@ -192,27 +193,7 @@ int main()
   glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
   // ウィンドウを作成する
-  GLFWwindow *const window(glfwCreateWindow(640, 480, "Hello!", NULL, NULL));
-  if (window == NULL)
-  {
-    // ウィンドウが作成できなかった
-    std::cerr << "Can't create GLFW window." << std::endl;
-    return 1;
-  }
-
-  // 作成したウィンドウを OpenGL の処理対象にする
-  glfwMakeContextCurrent(window);
-
-  // GLEW を初期化する
-  if (glewInit() != GLEW_OK)
-  {
-    // GLEW の初期化に失敗した
-    std::cerr << "Can't initialize GLEW" << std::endl;
-    return 1;
-  }
-
-  // 作成したウィンドウに対する設定
-  glfwSwapInterval(1);
+  Window window;
 
   // 背景色を指定する
   glClearColor(1.0f, 1.0f, 1.0f, 0.0f);
@@ -224,7 +205,7 @@ int main()
   std::unique_ptr<const Shape> shape(new Shape(2, 4, rectangleVertex));
 
   // ウィンドウが開いている間繰り返す
-  while (glfwWindowShouldClose(window) == GL_FALSE)
+  while (window)
   {
     // ウィンドウを消去する
     glClear(GL_COLOR_BUFFER_BIT);
@@ -235,10 +216,7 @@ int main()
     // 図形を描画する
     shape->draw();
 
-    // カラーバッファを入れ替える
-    glfwSwapBuffers(window);
-
-    // イベントを取り出す
-    glfwWaitEvents();
+    // カラーバッファを入れ替えてイベントを取り出す
+    window.swapBuffers();
   }
 }
