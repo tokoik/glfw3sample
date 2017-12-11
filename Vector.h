37af0ecc9f3e0a74a2552d8df7489cb1b9d748e6
Kohe Tokoi <tokoik@gmail.com>
2017-12-11 23:47:29 +0900

    step35

---
+++ Vector.h
@@ -0,0 +1,25 @@
+﻿#pragma once
+#include <array>
+
+// 変換行列
+#include "Matrix.h"
+
+//
+// ベクトル
+//
+using Vector = std::array<GLfloat, 4>;
+
+// 行列とベクトルの乗算
+//   m: Matrix 型の行列
+//   v: Vector 型のベクトル
+Vector operator*(const Matrix &m, const Vector &v)
+{
+  Vector t;
+
+  for (int i = 0; i < 4; ++i)
+  {
+    t[i] = m[i] * v[0] + m[i + 4] * v[1] + m[i + 8] * v[2] + m[i + 12] * v[3];
+  }
+
+  return t;
+}
