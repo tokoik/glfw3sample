f9d50f490e008ad5c24a83f4f26ae8d6bee40b6f
Kohe Tokoi <tokoik@gmail.com>
2017-12-19 23:33:15 +0900

    step37

---
+++ Material.h
@@ -0,0 +1,21 @@
+﻿#pragma once
+#include <array>
+#include <GL/glew.h>
+
+//
+// 材質データ
+//
+struct Material
+{
+  // 環境光の反射係数
+  alignas(16) std::array<GLfloat, 3> ambient;
+
+  // 拡散反射係数
+  alignas(16) std::array<GLfloat, 3> diffuse;
+
+   // 鏡面反射係数
+  alignas(16) std::array<GLfloat, 3> specular;
+
+  // 輝き係数
+  alignas(4) GLfloat shininess;
+};
